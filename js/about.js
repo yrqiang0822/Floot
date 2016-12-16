4988aafe2b016a5bc2c41bc2ce1da07657a9ec7a
Rongqiang Yao <289440478@qq.com>
2016-12-16 17:08:52 +0800

    update

---
+++ js/about.js
@@ -0,0 +1,14 @@
+var bagData = [
+	{
+		url:"../images/2.jpg"
+	},
+	{
+		url:"../images/3.jpg"
+	}
+];
+function createBag(option) {
+	var data = option.data,
+		datalen = data.length,
+		element = document.getElementById(option.element),
+
+}
